b331532eefbebc19613a48d7b9d9a1de62266b2e
qizhouduan-nd <duanqizhou@gmail.com>
2023-08-20 22:31:44 -0400

    Update ptg_main_analysis.r

--- ptg_main_analysis.r
+++ ptg_main_analysis.r
@@ -1,11 +1,16 @@
 ###############################
 ### ptg main analysis script
 ###############################
+library(tidyverse)
+ptg_effect_sizes <- read_excel("Desktop/ptg effect sizes.xlsx")
+View(ptg_effect_sizes)   
 
 
+ptg_effect_sizes %>% select(`scale type` )
 
+ptgi_studies = ptg_effect_sizes %>% filter(`scale type` == 'PTGI')
 
-
+as.numeric(ptgi_studies$`effect size`)
 
 
 
