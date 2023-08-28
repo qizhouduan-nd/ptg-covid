a0b4a3e142be7e4c37085088ddfb5bb10522e7d3
qizhouduan-nd <duanqizhou@gmail.com>
2023-08-28 09:20:00 -0400

    update

--- ptg_main_analysis.r
+++ ptg_main_analysis.r
@@ -14,10 +14,18 @@ main_data = merge(shortlist, t1_raw,by = 'Source') %>%
   select(Source, 'scale type', 'effect size', 'sd', 'sample size', 
          'Groups with PTG', 'Countries of Origin')
 
-View(main_data)
+## look at how many studies uses PTGI
+main_data %>% group_by(`scale type`) %>% count() # 13 effect sizes uses PTGI 6 uses PTGI-SF
 
+## we can do normalization or we can perform separate analysis for these two types of studies
+## check the sample size
+sum(main_data$`sample size`) ## overall
+PTGI_dat = main_data %>% filter(`scale type` == 'PTGI')
+PTGISF_dat = main_data %>% filter(`scale type` != 'PTGI')
+sum(PTGI_dat$`sample size`)
+sum(PTGISF_dat$`sample size`)
 
-
+## let's do meta analysis for the PTGI studies
 
 
 
