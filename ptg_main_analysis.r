6f9044f918d79333ec3f810c2b49b11c9cc4a433
qizhouduan-nd <duanqizhou@gmail.com>
2023-08-28 01:52:48 -0400

    updated r markdown

--- ptg_main_analysis.r
+++ ptg_main_analysis.r
@@ -5,10 +5,16 @@ library(tidyverse)
 library(readxl)
 setwd(dirname(rstudioapi::documentPath()))
 
-read_excel('ptg_shortlist.xlsx')
+## load data
+shortlist = read_excel('ptg_shortlist.xlsx')
+t1_raw = read_excel('Covid T1 Raw.xlsx')
 
+## select variables for main analysis
+main_data = merge(shortlist, t1_raw,by = 'Source') %>% 
+  select(Source, 'scale type', 'effect size', 'sd', 'sample size', 
+         'Groups with PTG', 'Countries of Origin')
 
-
+View(main_data)
 
 
 
