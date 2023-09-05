420627d5e9419ad9acd2e78c8249d455478e77c8
qizhouduan-nd <duanqizhou@gmail.com>
2023-09-04 23:59:27 -0400

    update

--- ptg_main_analysis.r
+++ ptg_main_analysis.r
@@ -13,7 +13,7 @@ t1_raw = read_excel('Covid T1 Raw.xlsx')
 
 ## select variables for main analysis
 main_data = merge(shortlist, t1_raw,by = 'Source') %>% 
-  select(Source, 'scale type', 'effect size', 'sd', 'sample size', 
+  dplyr::select('Source', 'scale type', 'effect size', 'sd', 'sample size', 
          'Groups with PTG', 'Countries of Origin')
 
 ## look at how many studies uses PTGI
